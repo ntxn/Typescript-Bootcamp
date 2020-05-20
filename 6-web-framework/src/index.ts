158a82963d052e6b57d3a44329968e192f901fdf
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-20 09:14:25 -0700

    Web-Framework: Implements a generic Collection and create a User Collection in User class

--- 6-web-framework/src/index.ts
+++ 6-web-framework/src/index.ts
@@ -1,10 +1,7 @@
 import { User } from './models/User';
 
-// const user = User.buildUser({ id: 1 });
-// user.on('change', () => console.log(user));
-// user.fetch();
+const collection = User.buildUserCollection();
 
-const user = User.buildUser({ name: 'User 2', age: 20 });
-user.on('save', () => console.log(user));
+collection.on('change', () => console.log(collection));
 
-user.save();
+collection.fetch();
--- 6-web-framework/src/models/User.ts
+++ 6-web-framework/src/models/User.ts
@@ -2,8 +2,9 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
@@ -17,6 +18,12 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
