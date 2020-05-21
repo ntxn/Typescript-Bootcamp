9551d8c965394d77e598d7d6c767719f76b2e9a7
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-21 12:06:56 -0700

    Server: Twist express library to work with TS classes using advanced features like decorators, metadata

---
+++ 7-server/build/controllers/decorators/use.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.use = void 0;
+require("reflect-metadata");
+var MetadataKeys_1 = require("./MetadataKeys");
+function use(middleware) {
+    return function (target, key, desc) {
+        var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target, key) || [];
+        middlewares.push(middleware);
+        Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.middleware, middlewares, target, key);
+    };
+}
+exports.use = use;
--- 7-server/src/index.ts
+++ 7-server/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-import { router as loginRoutes } from './routes/loginRoutes';
+import { AppRouter } from './AppRouter';
+import './controllers/AuthController';
+import './controllers/RootController';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['cookieKey'] }));
-app.use(loginRoutes);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
